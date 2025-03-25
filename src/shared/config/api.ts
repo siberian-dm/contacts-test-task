13573415cc8801dbe7ca5889b50f54ca478820d7
Dmitry Vorobev <dm.siberian@gmail.com>
2025-03-25 17:26:08 +0300

    Update api.ts

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -1,7 +1,7 @@
 export const REQUEST_TIMEOUT = 60000;
 export const MAX_FAILED_REQUEST = 3;
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL || 'https://contacts-server-jiqq.onrender.com';
 
 export const TOKEN_TYPE = 'Bearer';
 export const REFRESH_TOKEN_PATH = 'refresh-token';
