3781d80acaeaf67a77508a5133888ced58391ec6
Dmitry Vorobev <dm.siberian@gmail.com>
2026-06-24 12:55:06 +0300

    fix: server port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     host: '0.0.0.0',
-    port: 3030,
+    port: 3031,
     allowedHosts: true,
   },
   build: {
